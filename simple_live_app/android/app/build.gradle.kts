b8269d100da4a959e1bb85fba09757d1eae08bad
dqsy20192202 <115028688+dqsy20192202@users.noreply.github.com>
2026-03-04 00:16:23 +0800

    Update build.gradle.kts

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -39,16 +39,16 @@ android {
         versionName = flutter.versionName
     }
 
-#   signingConfigs {
-  #      create("release") {
-   #         keyAlias = keystoreProperties["keyAlias"] as String
-   #         keyPassword = keystoreProperties["keyPassword"] as String
-   #         storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-    #        storePassword = keystoreProperties["storePassword"] as String
-   #         isV1SigningEnabled = true
- #           isV2SigningEnabled = true
-#        }
-#    }
+//   signingConfigs {
+  //      create("release") {
+   //         keyAlias = keystoreProperties["keyAlias"] as String
+   //         keyPassword = keystoreProperties["keyPassword"] as String
+   //         storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+    //        storePassword = keystoreProperties["storePassword"] as String
+   //         isV1SigningEnabled = true
+ //           isV2SigningEnabled = true
+//        }
+//    }
 
     buildTypes {
         release {
